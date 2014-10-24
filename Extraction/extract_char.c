4a7290087e3322b62ce1d3908a1897441ec53a82
d0tiKs <alessandro.pisu@epita.fr>
2014-10-24 22:22:03 +0200

    Note efficient commit for Extraction

--- Extraction/extract_char.c
+++ Extraction/extract_char.c
@@ -1,1 +1,6 @@
+#include "extract_char.h"
 
+void getPixelByCollumn( SDL_Surface* img, Line l)
+{
+	
+}
--- Extraction/extract_char.h
+++ Extraction/extract_char.h
@@ -1,1 +1,33 @@
+#ifndef STDLIB_STDIO
+#define STDLIB_STDIO
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+#endif
+
+#ifndef EXTRACT_CHAR
+#define EXTRACT_CHAR
+
+typedef struct{
+	int x,y;
+	int weight,hight;
+}TextBloc;
+
+typedef struct{
+	int hight;
+	int x,y;
+}Line;
+
+typedef struct{
+	int x,y;
+	CharList *next;
+
+}CharNode;
+
+void getPixelByCollumn( SDL_Surface* img, Line l);
+
+#endif
