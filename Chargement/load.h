9f62773dfde845d94e83187ff09c71cae983bf3b
d0tiKs <alessandro.pisu@epita.fr>
2014-10-26 03:21:31 +0100

    Update load.h

--- Chargement/load.h
+++ Chargement/load.h
@@ -1,8 +1,19 @@
+#ifndef STDLIB_STDIO
+#define STDLIB_STDIO
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
+#include <math.h>
+
+#endif
+
+#ifndef LOAD
+#define LOAD
 
 int isFileExists(const char *path);
 
 SDL_Surface* loadImage (char path[]);
+
+#endif
