648c3d41b0f183771a4a34a28a9387d5a4e91082
meghane <meghane@users.noreply.github.com>
2014-12-07 19:04:43 +0100

    Update rotation.c

--- Pretraitement/rotation.c
+++ Pretraitement/rotation.c
@@ -20,11 +20,60 @@ void rotation(SDL_Surface *img, double angle)
         {
                 for(int y = 0; y<img->h; y++)
                 {
-                        if(getpixel(img, x, y) == 0/*xffffffff*/)
+                        if(getpixel(img, x, y) == 0)
                                 putpixel(new_img, abs(lrint(centre_x + (x - centre_x)*cosf(-angle) - (y - centre_y)*sinf(-angle))), abs(lrint(centre_y + (x - centre_x)*sinf(-angle) + (y - centre_y)*cosf(-angle))),
                                         getpixel(img, x, y));
                 }
         }
         //free(img);
         img = new_img;
+}
+
+void detection(SDL_Surface *image)
+{
+        long double mat[image->h], var[15];
+        long double  moyenne = 0, variance = 0, angle = 0;
+        int max_variance = 0;
+        SDL_Surface *img;
+        Uint32 pixel;
+
+        for(int x =0; x<image->h; x++) //initialisation de la matrice
+                mat[x] = 0;
+
+        for(int a = 0; a<15; a++) //test pour chaque angle
+        {
+                img  = SDL_ConvertSurface(image, image->format, image->flags);
+                rotation(img, M_PI * (a) / 180);
+                for(int i =0; i<img->h; i++) //compte le nb de pixel noir
+                {
+                        for(int j = 0; j <img->w; j++)
+                        {
+                                pixel = getpixel(img, j, i);
+                                if(pixel  != 0xffffffff)
+                                        mat[i] = mat[i] + 1;
+                        }
+                }
+
+                for(int i = 0; i < img->h ; i++) //moyenne pixel noir
+                {
+                        moyenne = mat[i] + moyenne;
+                }
+                moyenne = moyenne/img->h;
+
+
+                for(int i = 0; i< img->h ; i++) //variance
+                {
+                        variance = variance + (((mat[i] - moyenne)*(mat[i] - moyenne))/img->h);
+                }
+                var[a] = variance;
+                free(img);
+        }
+
+        for(int l = 0; l < img->h ; l++) //variance max
+        {
+                if(var[max_variance] <= var[l])
+                        max_variance = l;
+        }
+        angle = max_variance; //angle correspondant
+        rotation(image, M_PI * (angle) / 180); //rotation definitive
 }
